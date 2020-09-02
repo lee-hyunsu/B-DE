57551956baec24ce72d0600944a57efe94bd75de
lee-hyunsu <dlgustn675@naver.com>
2020-09-02 23:51:29 +0900

    수정

--- src/main.js
+++ src/main.js
@@ -1,11 +1,15 @@
-let bigPic = document.querySelector("#big");
-let smallPics = document.querySelectorAll(".small");
+// let a = prompt(alert("이름을 입력해주세요.", ""));
 
-for(let i = 0; i < smallPics.length; i++){
-    smallPics[i].addEventListener("click", mainpic);
-}
 
-function mainpic(){
-    let smallPicAttribute = this.getAttribute("src");
-    bigPic.setAttribute("src", smallPicAttribute);
-}
+
+// let bigPic = document.querySelector("#big");
+// let smallPics = document.querySelectorAll(".small");
+
+// for(let i = 0; i < smallPics.length; i++){
+//     smallPics[i].addEventListener("click", mainpic);
+// }
+
+// function mainpic(){
+//     let smallPicAttribute = this.getAttribute("src");
+//     bigPic.setAttribute("src", smallPicAttribute);
+// }
