b04291593a695a660aa3bfce1872b6fd1f0e27e3
lee-hyunsu <dlgustn675@naver.com>
2020-09-02 23:28:05 +0900

    수정

--- src/main.js
+++ src/main.js
@@ -2,10 +2,10 @@ let bigPic = document.querySelector("#big");
 let smallPics = document.querySelectorAll(".small");
 
 for(let i = 0; i < smallPics.length; i++){
-    smallPics[i].addEventListener("click", changepic);
+    smallPics[i].addEventListener("click", mainpic);
 }
 
-function changepic(){
+function mainpic(){
     let smallPicAttribute = this.getAttribute("src");
     bigPic.setAttribute("src", smallPicAttribute);
 }
